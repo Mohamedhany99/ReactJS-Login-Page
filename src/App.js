21212a7507597189d19ede160b2c25d561e9d030
Mohamed Hany <Mohamed.hany182@outlook.com>
2022-10-26 13:37:26 +0200

    login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {useState} from 'react';
+import Login from './components/Login';
 function App() {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "pass123"
+  }
+  const [user,setUser] = useState({name:"",email:""});
+  const [error,setError] = useState("");
+  const login = details => {
+    console.log(details);
+  }
+  const logout = () =>{
+    console.log("logout");
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email !="")? (
+        <div className='welcome'>
+          <h2> welcome, <span>{user.name}</span></h2>
+          <button>Logout</button>
+        </div>
+        // <Login/>
+      ):(
+        <Login/>
+      )}
     </div>
   );
 }
